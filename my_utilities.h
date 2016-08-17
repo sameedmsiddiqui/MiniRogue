eb44d699c3f7f0d012eba40c88aea568d3eb7fa3
sameedmsiddiqui <sameedmsiddiqui@gmail.com>
2016-08-17 16:46:49 -0700

    Delete my_utilities.h

--- my_utilities.h
+++
@@ -1,8 +0,0 @@
-#ifndef MY_UTILITIES_H
-#define MY_UTILITIES_H
-#include "utilities.h"
-
-// return a positive integer between a and b, inclusive
-int randInt(int a, int b);
-
-#endif//MY_UTILITIES_H
